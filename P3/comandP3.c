43c350cc44a29f9e501a5baef89c561cf041c9e4
Adega <eadegafernandez@gmail.com>
2022-12-01 08:59:44 +0100

    modified:   P3/comandP3.c 	modified:   P3/comandP3.h 	modified:   P3/comandP2.c 	deleted:    shell-referencia/FreeBSD/shell 	deleted:    shell-referencia/FreeBSD/shell-32bit 	deleted:    shell-referencia/FreeBSD/shell-32bit-static 	deleted:    shell-referencia/FreeBSD/shell-static 	deleted:    shell-referencia/LINUX/Text file 	deleted:    shell-referencia/LINUX/shell 	deleted:    shell-referencia/LINUX/shell-32bit 	deleted:    shell-referencia/LINUX/shell-32bit-static 	deleted:    shell-referencia/LINUX/shell-static 	deleted:    shell-referencia/MAC-OS/shell-mac 	deleted:    shell-referencia/MAC-OS/shell-mac-M1 	P3/ayudaP3.c 	shell-referencia/ShellReferencia/

---
+++ P3/comandP3.c
@@ -0,0 +1,5 @@
+#include "comandP3.h"
+
+void priority(char* Arg[], int numA){
+    
+}
---
+++ P3/comandP3.h
@@ -0,0 +1,16 @@
+#ifndef COMAND_P3_H
+#define COMAND_P3_H
+
+#include "includes.h"
+
+void priority();
+void showvar();
+void changevar();
+void showenv();
+void forkShell();
+void execute();
+void listjobs();
+void deljobs();
+void job();
+
+#endif
