e213631247f1593060adc2e85ed07302898264ef
Adega <eadegafernandez@gmail.com>
2022-12-01 10:28:02 +0100

    modified:   P3/comandP3.c

--- P3/comandP3.c
+++ P3/comandP3.c
@@ -28,3 +28,30 @@ void forkShell(){
 	}else if (pid!=-1) waitpid (pid,NULL,0);
     else perror("error");
 }
+
+/*void execute(char* Arg[], int numA){
+    char* almaaux[TROCEO-1];
+    strcpy(almaaux, Arg+sizeof(Arg[0]));
+}
+
+char * Ejecutable (char *s){
+	char path[MAXNAME];
+	static char aux2[MAXNAME];
+	struct stat st;
+	char *p;
+
+	if (s==NULL || (p=getenv("PATH"))==NULL) return s;
+
+	if (s[0]=='/' || !strncmp (s,"./",2) || !strncmp (s,"../",3)) return s;       //is an absolute pathname
+
+	strncpy (path, p, MAXNAME);
+	for (p=strtok(path,":"); p!=NULL; p=strtok(NULL,":")){
+       sprintf (aux2,"%s/%s",p,s);
+	   if (lstat(aux2,&st)!=-1) return aux2;
+	}
+	return s;
+}
+
+int OurExecvpe(const char *file, char *const argv[], char *const envp[]){
+   return (execve(Ejecutable(file) ,argv, envp));
+}*/
